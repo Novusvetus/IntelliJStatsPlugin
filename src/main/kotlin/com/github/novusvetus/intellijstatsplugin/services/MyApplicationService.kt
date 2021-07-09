9862c272c830f8eacafb4ef05e487ba99502df5c
GitHub Action <action@github.com>
2021-07-09 11:53:03 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/novusvetus/intellijstatsplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.novusvetus.intellijstatsplugin.services
+
+import com.github.novusvetus.intellijstatsplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/novusvetus/intellijstatsplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/novusvetus/intellijstatsplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.novusvetus.intellijstatsplugin.services
 
+import com.github.novusvetus.intellijstatsplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
